12049481f2e49cd6c664893df5b7011440ecfc55
Joha-will <johawilsonatu@icloud.com>
2022-06-29 13:55:22 +0000

    Created quiz html and added html structure for question header and answer buttons

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+// Declaring variables to get elements
+const startButton = document.getElementById('start-button');
+const aboutButton = document.getElementById('about-button');
+const mainButtons = document.getElementsByClassName('main-buttons');
+
+
+
+
+// Function for the quiz games
+
+function startGame() {
+
+}
+
+function removeButton() {
+    
+}
