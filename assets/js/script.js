a66b5d0b10ee9e0d9ee66614580ad4595b51196b
Joha-will <johawilsonatu@icloud.com>
2022-06-30 13:55:13 +0000

    I have added an object of array with question to js file to carry out a few tests

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,9 +8,40 @@ const textArea = document.getElementById('quiz-info');
 let quizInfo = `
 
     <h2> How to play </h2>
+    <p> To start game just simply click the <strong style="background-color: greenyellow"> Start Quiz Button.</strong></p>
+    <p> Choose the answer that you think is correct.</p>
     <p> Once you have seleted your answer, <strong style="background-color: greenyellow"> you can not change it.</strong></p>
-    <p> Each correct answer is worth 10 points.</p>
+    <p> Each correct answer is worth <strong style="background-color: greenyellow"> 10 points.</strong></p>
 `;
+
+
+
+// Questions to display to users.
+const questions = [
+    {
+        question: 'What year was Nottingham Forest FC founded?',
+        option1: '1879',
+        option2: '1906',
+        option3: '1865',
+        answer: 3,
+    },
+    {
+        question: 'What did Nottingham Forest win in 1979?',
+        option1: 'Premier League',
+        option2: 'European Cup Final',
+        option3: 'Carabao Cup Final',
+        answer:  2,
+    },
+    {
+        question: 'what year did Steve Cooper take over as Nottingham Forest manager?',
+        option1: '1993',
+        option2: '2001',
+        option3: '2021',
+        answer: 3,
+    },
+]
+
+
 
 
 
@@ -32,7 +63,7 @@ function checkAnswer() {
 }
 
 function incrementScore() {
-    
+
 }
 
 
