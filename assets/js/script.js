a22399857b5d25b495902c99356a4260d036623b
Joha-will <johawilsonatu@icloud.com>
2022-06-30 01:36:14 +0000

    I have started declaring variables, creating functions and creating elements in javascript file

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,15 @@ const startButton = document.getElementById('start-button');
 const aboutButton = document.getElementById('about-button');
 const mainButtons = document.getElementsByClassName('main-buttons');
 
+const textArea = document.getElementById('quiz-info');
+
+let quizInfo = `
+    <h2> How to play </h2>
+    <p> Once you have seleted your answer, <strong> style="background-color: greenyellow" you can not change it.</strong></p>
+    <br>
+    <p> Each correct answer is worth 10 points.</p>
+`;
+
 
 
 
@@ -12,6 +21,12 @@ function startGame() {
 
 }
 
+function aboutQuiz() {
+    const homeButton = document.createElement('a');
+    const homeButtonText = document.createTextNode('Home')
+
+}
+
 function removeButton() {
-    
+    mainButtons.remove()
 }
