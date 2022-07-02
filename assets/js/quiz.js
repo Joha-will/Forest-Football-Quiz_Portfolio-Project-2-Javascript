f908a54d2b0d5236d16228406da2a50fa97ba007
Joha-will <johawilsonatu@icloud.com>
2022-07-02 12:55:10 +0000

    i adjust the score display with css and added more function controls to the display questions function

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,3 +1,4 @@
+// These are the questions that would be displayed to the users of the quiz
 const questions = [
     {
         question: 'What year was Nottingham Forest founded?',
@@ -22,18 +23,36 @@ const questions = [
     }
 ]
 
+// Getting elements from the DOM and assigning them to variables
+
 const questionContain = document.getElementById('question-container');
 const answerButtons = document.getElementById('get-buttons');
 
 
+// Variables created to use within the functions
+let scoreIndex = 0;
+let randomQuestion;
+
+// Adding Eventlisteners
+document.addEventListener('DOMContentLoaded', startQuiz())
 
 
+// Functions to play and control the quiz game
 
-document.addEventListener('DOMContentLoaded', displayQuestions())
+function startQuiz(){
+    randomQuestion = questions.sort(() => Math.random() - .5);
+    displayQuestions()
+}
 
 function displayQuestions() {
     let askedQuestions = `
     <h2 id="display-questions"> Question: ${questions[0].question}</h2>
-`;
-questionContain.innerHTML = askedQuestions;
+    `;
+    questionContain.innerHTML = askedQuestions;
+    let ListOfBtn = `
+    <button type="button" class="answer-btn" data-number="1"> ${questions[0].option1}</button>
+    <button type="button" class="answer-btn" data-number="2"> ${questions[0].option2}</button>
+    <button type="button" class="answer-btn" data-number="3"> ${questions[0].option3}</button>
+    `;
+    answerButtons.innerHTML = ListOfBtn;
 }
